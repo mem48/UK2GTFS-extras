fcb76abcea244bf6a95722bdcf2401edd816ba10
Malcolm Morgan <mem48@users.noreply.github.com>
2019-08-16 16:48:55 +0100

    testing updates

--- RScripts/test_transxchange.R
+++ RScripts/test_transxchange.R
@@ -3,7 +3,7 @@ code <- list.files("../UK2GTFS/R", full.names = T)
 for(i in code){source(i)}
 
 #Find Files
-dir = "E:/OneDrive - University of Leeds/Routing/TransitExchangeData/data_20180515/SW/"
+dir = "E:/OneDrive - University of Leeds/Routing/TransitExchangeData/data_20180515/L/"
 files = list.files(dir, full.names = T, recursive = T, pattern = ".xml")
 #file = "E:/OneDrive - University of Leeds/Routing/TransitExchangeData/data_20180515/SW/swe_43-n1-_-y10-1.xml"
 #file = files[381]
@@ -13,19 +13,19 @@ naptan = get_naptan()
 cal = get_bank_holidays()
 
 
-x = 206
+x = 2
 res_single = transxchange_import(file = files[x], run_debug = run_debug, full_import = full_import)
 gtfs_single = transxchange_export(obj = res_single, run_debug = T, cal = cal, naptan = naptan)
 write_gtfs(gtfs = gtfs_single, folder = "../mem48/UK2GTFS-extras/export", name = gsub(".xml","",strsplit(files[x], "/")[[1]][7]))
 
 
 y = 1:length(files)
-y = 200:300
+y = 300:400
 res_batch  = pbapply::pblapply(files[y], transxchange_import, run_debug = run_debug, full_import = full_import)
 gtfs_batch = pbapply::pblapply(res_batch, transxchange_export, run_debug = T, cal = cal, naptan = naptan)
-foo = lapply(gtfs_batch, gtfs_validate_internal)
+# foo = lapply(gtfs_batch, gtfs_validate_internal)
 gtfs_merged <- gtfs_merge(gtfs_batch)
-write_gtfs(gtfs = gtfs_merged, folder = "extras", name = "EA_nobook")
+write_gtfs(gtfs = gtfs_merged, folder = "../mem48/UK2GTFS-extras/export", name = "SW")
 
 
 res_all  = pbapply::pblapply(files, transxchange_import, run_debug = run_debug, full_import = full_import)
